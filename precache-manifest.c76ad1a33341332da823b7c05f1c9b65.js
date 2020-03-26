9750347734dbfb58f87dc84a5aa39ea67b49f556
piotr so <43079726+piotr-so@users.noreply.github.com>
2020-03-26 19:52:27 +0100

    Updates

---
+++ precache-manifest.c76ad1a33341332da823b7c05f1c9b65.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88051fe00214ee5f4b3635157cc3e1bb",
+    "url": "/f1-app/index.html"
+  },
+  {
+    "revision": "c8f4568db35e40aa3907",
+    "url": "/f1-app/static/css/main.d479bee4.chunk.css"
+  },
+  {
+    "revision": "8348c3b02a8e419015fa",
+    "url": "/f1-app/static/js/2.20327b6d.chunk.js"
+  },
+  {
+    "revision": "61e9854a6bf57dc077cc06fd0e1da9b1",
+    "url": "/f1-app/static/js/2.20327b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f4568db35e40aa3907",
+    "url": "/f1-app/static/js/main.ea7fe1ea.chunk.js"
+  },
+  {
+    "revision": "954a8b0f00b688cd4f3f",
+    "url": "/f1-app/static/js/runtime-main.f5a7ab84.js"
+  },
+  {
+    "revision": "95ac8be98958406fe0c3a571efee2d75",
+    "url": "/f1-app/static/media/constructors.95ac8be9.svg"
+  },
+  {
+    "revision": "27bb001bb929d4f1d79eb93bbd02be67",
+    "url": "/f1-app/static/media/drivers.27bb001b.svg"
+  },
+  {
+    "revision": "43eb989e2d3ad7070e653df1d4634151",
+    "url": "/f1-app/static/media/f1_logo_white.43eb989e.svg"
+  },
+  {
+    "revision": "ce9512153dcd5b5bef044f35f5120547",
+    "url": "/f1-app/static/media/home.ce951215.svg"
+  },
+  {
+    "revision": "3154fa8b6a4bfeb8047615b199b77123",
+    "url": "/f1-app/static/media/left-arrow.3154fa8b.svg"
+  },
+  {
+    "revision": "a60548f754540b20acfc05043c6185a2",
+    "url": "/f1-app/static/media/podium.a60548f7.svg"
+  },
+  {
+    "revision": "27ddc0cb438a793de70d29ca5a4c2837",
+    "url": "/f1-app/static/media/race-track.27ddc0cb.svg"
+  },
+  {
+    "revision": "00448c17968d16ead9b3f98d3e951b1f",
+    "url": "/f1-app/static/media/racing-car.00448c17.svg"
+  },
+  {
+    "revision": "e412b94523915174061d67bef6736c26",
+    "url": "/f1-app/static/media/smartphone.e412b945.png"
+  },
+  {
+    "revision": "ac2a2a42608c2f55438556e23a20a90d",
+    "url": "/f1-app/static/media/trophy.ac2a2a42.svg"
+  }
+]);
